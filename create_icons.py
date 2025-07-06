8dfcc556c1a38f6003bd013eb077d2954f8d43d9
wieserd <35579566+wieserd@users.noreply.github.com>
2025-07-06 12:28:21 +0200

    Add files via upload

--- create_icons.py
+++ create_icons.py
@@ -1,1 +1,9 @@
+from PIL import Image
 
+def create_icon(size, color, file_path):
+  img = Image.new('RGB', (size, size), color = color)
+  img.save(file_path)
+
+create_icon(16, 'black', 'icons/icon16.png')
+create_icon(48, 'black', 'icons/icon48.png')
+create_icon(128, 'black', 'icons/icon128.png')
