a31fa9eb341971d1372e9474ce2cccc351dbcb3c
wieserd <35579566+wieserd@users.noreply.github.com>
2025-07-06 12:24:12 +0200

    Update create_icons.py

--- create_icons.py
+++ create_icons.py
@@ -1,9 +1,1 @@
-from PIL import Image
 
-def create_icon(size, color, file_path):
-  img = Image.new('RGB', (size, size), color = color)
-  img.save(file_path)
-
-create_icon(16, 'black', '/Users/dw2022/gemini/icons/icon16.png')
-create_icon(48, 'black', '/Users/dw2022/gemini/icons/icon48.png')
-create_icon(128, 'black', '/Users/dw2022/gemini/icons/icon128.png')
